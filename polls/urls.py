728e8f5a28bc09b3e2a64b47ef6861989eaab3a3
Sankalp Mukim <sankalpmukim@gmail.com>
2021-03-06 08:19:13 +0000

    config fixing

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('test_form/', views.test_form, name='form'),
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('', views.test_form, name='form'),
     path('test_result/', views.test_result, name='result'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
