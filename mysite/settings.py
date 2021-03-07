58b977a75704a544f30fade040856b19ac9980fb
sankalpmukim <sankalpmukim@gmail.com>
2021-03-07 09:44:03 +0530

    import os

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
